050fa2a893d5a1f621c2b68c1ff3961c04fcdebc
atrur nikitin <branewor@gmail.com>
2021-12-23 13:55:11 +0400

    feat: Прописать логику на JS для попапа

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,12 @@ let page = document.querySelector('.page');
 let openBtn = document.querySelector('.profile__edit-button');
 let form = document.querySelector('.pop-up')
 let closeBtn = document.querySelector('.pop-up__close-btn');
+let formElement = document.querySelector('.pop-up__body');
 
 let profileName = document.querySelector('.profile__name')
 let profileDescr = document.querySelector('.profile__description')
 let formName = document.querySelector('.pop-up__name')
-let formDescr = document.querySelector('.pop-up__desct')
-
-
+let formDescr = document.querySelector('.pop-up__descr')
 
 openBtn.addEventListener('click', () => {
     form.classList.add('pop-up_enabled');
@@ -22,3 +21,18 @@ closeBtn.addEventListener('click', () => {
     form.classList.remove('pop-up_enabled');
     page.classList.remove('page_stop-scroll')
 })
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileName.textContent = formName.value;
+    profileDescr.textContent = formDescr.value;
+    console.log(111);
+}
+
+formElement.addEventListener('submit', (e) => {
+    e.preventDefault();
+    profileName.textContent = formName.value;
+    profileDescr.textContent = formDescr.value;
+    form.classList.remove('pop-up_enabled');
+    page.classList.remove('page_stop-scroll')
+});
