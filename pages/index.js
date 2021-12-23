fbfa0cf17134c2b4acf3eca8817576f9e4506959
atrur nikitin <branewor@gmail.com>
2021-12-23 13:35:09 +0400

    fix: переделать инконку в секции prodile

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,20 @@ let openBtn = document.querySelector('.profile__edit-button');
 let form = document.querySelector('.pop-up')
 let closeBtn = document.querySelector('.pop-up__close-btn');
 
+let profileName = document.querySelector('.profile__name')
+let profileDescr = document.querySelector('.profile__description')
+let formName = document.querySelector('.pop-up__name')
+let formDescr = document.querySelector('.pop-up__desct')
 
-console.log(closeBtn);
-console.log(page);
+
 
 openBtn.addEventListener('click', () => {
     form.classList.add('pop-up_enabled');
     page.classList.add('page_stop-scroll')
+    formName.value = profileName.textContent;
+    formDescr.value = profileDescr.textContent;
 });
+
 
 closeBtn.addEventListener('click', () => {
     form.classList.remove('pop-up_enabled');
