2d783f852d8b1c3426889298ef647714906e1734
artur nikitin <branewor@gmail.com>
2022-01-01 10:37:24 +0400

    hotfix: Пофиксить js код + добавить орграничение ширины в css prodile__info

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,31 @@
-let page = document.querySelector('.page');
-let openBtn = document.querySelector('.profile__edit-button');
-let form = document.querySelector('.pop-up')
-let closeBtn = document.querySelector('.pop-up__close-btn');
+let formModal = document.querySelector('.pop-up')
 let formElement = document.querySelector('.pop-up__body');
+let openBtn = document.querySelector('.profile__edit-button');
+let closeBtn = document.querySelector('.pop-up__close-btn');
 
 let profileName = document.querySelector('.profile__name')
 let profileDescr = document.querySelector('.profile__description')
 let formName = document.querySelector('.pop-up__name')
 let formDescr = document.querySelector('.pop-up__descr')
 
+function modalToggler() {
+  formModal.classList.toggle('pop-up_enabled');
+}
+
 openBtn.addEventListener('click', () => {
-    form.classList.add('pop-up_enabled');
-    page.classList.add('page_stop-scroll')
-    formName.value = profileName.textContent;
-    formDescr.value = profileDescr.textContent;
+  modalToggler()
+  formName.value = profileName.textContent;
+  formDescr.value = profileDescr.textContent;
 });
 
 
 closeBtn.addEventListener('click', () => {
-    form.classList.remove('pop-up_enabled');
-    page.classList.remove('page_stop-scroll')
+  modalToggler()
 })
 
 formElement.addEventListener('submit', (e) => {
     e.preventDefault();
     profileName.textContent = formName.value;
     profileDescr.textContent = formDescr.value;
-    form.classList.remove('pop-up_enabled');
-    page.classList.remove('page_stop-scroll')
+    formModal.classList.remove('pop-up_enabled');
 });
